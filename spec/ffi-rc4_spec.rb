18e1883a57cafc81bb2d13086d1bb8e0e3226bb1
Brandon Dewitt <brandonsdewitt@gmail.com>
2016-08-21 15:02:11 -0600

    more basic usage specs

--- spec/ffi-rc4_spec.rb
+++ spec/ffi-rc4_spec.rb
@@ -12,4 +12,15 @@ describe FFI_RC4 do
   it 'decrypts a base64 value encrypted with the same secret' do
     expect(FFI_RC4::decrypt_base64(FFI_RC4::encrypt_base64("hello", "secret"), "secret")).to eq("hello")
   end
+
+  it 'respects the FFI_RC4.default value for secret key' do
+    FFI_RC4.default = "secret"
+    expect(FFI_RC4::decrypt_base64(FFI_RC4::encrypt_base64("hello"))).to eq("hello")
+    FFI_RC4.default = nil
+  end
+
+  it 'raises an argment error if no secret or FFI_RC4.default is set' do
+    FFI_RC4.default = nil
+    expect { FFI_RC4::decrypt_base64(FFI_RC4::encrypt_base64("hello")) }.to raise_error(ArgumentError)
+  end
 end
